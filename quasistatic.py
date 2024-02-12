7a5b16bf55c330a01ad4a71ea8dc7977fe1d4d19
Ogier <seo@NIST.GOV>
2024-02-12 15:50:48 -0700

    Restructured Quasistatic

--- quasistatic.py
+++ quasistatic.py
@@ -4,85 +4,101 @@ from scipy.constants import mu_0, pi
 
 debug = False
 
-class ObsPoints:
-    def __init__(self, points) -> None:
-        self.points = points
+class Grid:
+    """Rectilinear grid class
+    Grid axes are along X, Y, and Z, and units are in meters
+    Grid doesn't have to have more than one point in each axis
+    Grid points don't have to be evenly spaced.
+    """
+    def __init__(self, Xs, Ys, Zs) -> None:
+        """Initialize Grid
+        X, Y, and Z are lists of points along those axes"""
+        self.Xs = Xs
+        self.Ys = Ys
+        self.Zs = Zs
     
-    @classmethod
-    def from_csv(cls, points_csv):
-        points = np.genfromtxt(points_csv, delimiter=',', skip_header=1)
-        return cls(points)
-
-    @classmethod
-    def plane(cls, width: tuple[float, float], step: float, orientation: str='XY', offset: float =0):
-        """Generate a rectangular grid in the XY plane at offset Z
-
-        width is (X, Y) dimensions in mm
-        step is sampling interval in mm
-        orientation is orientation of plane XY, XZ, YZ
-        offset is offset in mm
-        """
-        width_m = [w/1000 for w in width]
-        offset_m = offset/1000
-        step_m = step/1000
-        
-        if orientation == 'XY':
-            x = np.arange(-width_m[0]/2, width_m[0]/2+step_m, step_m)
-            y = np.arange(-width_m[1]/2, width_m[1]/2+step_m, step_m)
-            z = np.array(offset_m)
-        elif orientation == 'XZ':
-            x = np.arange(-width_m[0]/2, width_m[0]/2+step_m, step_m)
-            y = np.array(offset_m)
-            z = np.arange(-width_m[1]/2, width_m[1]/2+step_m, step_m)
-        elif orientation == 'YZ':
-            x = np.array(offset_m)
-            y = np.arange(-width_m[0]/2, width_m[0]/2+step_m, step_m)
-            z = np.arange(-width_m[1]/2, width_m[1]/2+step_m, step_m)
-
-
-        X, Y, Z = np.meshgrid(x,y,z)
+    @property
+    def points(self):
+        """Get list of (x, y, z) points
+        Result is actually 2D, but the second dimension is the XYZ components
+        [nX*nY*nZ, 3]"""
+        X, Y, Z = np.meshgrid(self.Xs,self.Ys,self.Zs)
         points = np.stack([X.ravel().T, Y.ravel().T, Z.ravel().T],1)
-
-        return cls(points)
-
-    @classmethod
-    def cube(cls, width: tuple[float, float, float], step: float):
-        """Generate a cubic rectilinear grid
-
-        width is (X, Y, Z) dimensions in mm
-        step is sampling interval in mm
-        """
-        width_m = [w/1000 for w in width]
-        step_m = step/1000
-        
-        x = np.arange(-width_m[0]/2, width_m[0]/2+step_m, step_m)
-        y = np.arange(-width_m[1]/2, width_m[1]/2+step_m, step_m)
-        z = np.arange(-width_m[2]/2, width_m[2]/2+step_m, step_m)
-
-        X, Y, Z = np.meshgrid(x,y,z)
-        points = np.stack([X.ravel().T, Y.ravel().T, Z.ravel().T],1)
-
-        return cls(points)
-    
-    @classmethod
-    def line(start, stop, points):
-        """Generate a line of evenly spaced points from start to stop
-
-        width is (X, Y, Z) dimensions in mm
-        step is sampling interval in mm
-        """
-        start_m = [s/1000 for s in start]
-        stop_m = [s/1000 for s in stop]
-        X = np.linspace(start_m[0], stop_m[0], points)
-        Y = np.linspace(start_m[1], stop_m[1], points)
-        Z = np.linspace(start_m[2], stop_m[2], points)
-        points = np.stack([X,Y,Z], axis=1)
         return points
 
-    def plot_points(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(*self.points.T)
+    @property
+    def shape(self):
+        """Get shape of grid"""
+        return [len(self.Xs), len(self.Ys), len(self.Zs)]
+    
+    @classmethod
+    def evenly_spaced_3D(cls, X_range=0, dX=0, Y_range=0, dY=0, Z_range=0, dZ=0):
+        """Generate grid of points evenly spaced in X, Y, Z
+        X/Y/Z_range are [start, stop] for each axis
+        dx/dY/dZ are step size for each axis
+        
+        If X, Y, or Z are single values or only 1 long
+        OR
+        if dX, dY, or dZ == 0
+        The the first (or only) value given for X, Y, or Z will be the only
+        value in that dimension for that grid.
+        """
+        # Make inputs into lists if they're not indexible
+        if type(X_range) not in (list, set, tuple):
+            X_range = [X_range]
+
+        if type(Y_range) not in (list, set, tuple):
+            Y_range = [Y_range]
+        
+        if type(Z_range) not in (list, set, tuple):
+            Z_range = [Z_range]
+
+        # If input is of length 1 or dX == 0, X is X_range[0]
+        # Similarly for Y and Z
+        if (len(X_range) == 1) or (dX == 0):
+            Xs = [X_range[0]]
+        else:
+            Xs = np.arange(X_range[0], X_range[1]+dX, dX)
+
+        if (len(Y_range) == 1) or (dY == 0):
+            Ys = [Y_range[0]]
+        else:
+            Ys = np.arange(Y_range[0], Y_range[1]+dY, dY)
+
+        if (len(Z_range) == 1) or (dZ == 0):
+            Zs = [Z_range[0]]
+        else:
+            Zs = np.arange(Z_range[0], Z_range[1]+dZ, dZ)
+
+        return cls(Xs, Ys, Zs)
+ 
+class Result(Grid):
+    def __init__(self, grid, values) -> None:
+        Grid.__init__(self, grid.Xs, grid.Ys, grid.Zs)
+        self.values = values
+    
+    @classmethod
+    def from_1D(cls, grid: Grid, values):
+        """Reshape [nX*nY*nZ, 3] list of points to [nX, nY, nZ, 3]
+        and return a results objkect."""
+        
+        nX = len(grid.Xs)
+        nY = len(grid.Ys)
+        nZ = len(grid.Zs)
+        shape = (nX, nY, nZ, 3)
+        print('New Shape:', shape)
+        values = values.reshape(shape)
+        return cls(grid, values)
+        
+    @property
+    def B1_mag(self):
+        """Calculate magnitude of XY component of values"""
+        return np.linalg.norm(self.values[..., :-1], axis=-1)
+    
+    @property
+    def B1_phase(self):
+        """Calculate phase of B1"""
+        return np.arctan2(self.values[...,1], self.values[...,0])
 
 class Coil:
     """MRI coil for quasistatic analysis.
@@ -117,10 +133,11 @@ class Coil:
         plt.show()
         return ax
     
-    def compute(self, obs: ObsPoints):
-        result = np.empty_like(obs.points)
-        for i in range(obs.points.shape[0]):
-            P = obs.points[i,:] # Observation Point
+    def compute_grid(self, grid: Grid) -> Result:
+        obs_points = grid.points
+        result_array = np.empty_like(obs_points)
+        for i in range(obs_points.shape[0]):
+            P = obs_points[i,:] # Observation Point
             l0s = self.points[0:-1,:]
             l1s = self.points[1:,:]
 
@@ -143,5 +160,8 @@ class Coil:
 
             phis = np.cross(self.l_hats, R_hats)
             summand = phis * (mu_0/(4*pi*R_mags)*(cos_theta_0+cos_theta_1))[:,None]
-            result[i,:] = np.sum(summand,0)
+            result_array[i,:] = np.sum(summand,0)
+        print(result_array.shape)
+        result = Result.from_1D(grid, result_array)
+        print(result.B1_mag.shape)
         return result
